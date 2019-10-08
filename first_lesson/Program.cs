588b513374d981f625fe1ba1cb1a565ea16ada8e
vlad.higher <vlad.higher@gmail.com>
2019-10-08 17:20:18 +0300

    draft1

---
+++ first_lesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
